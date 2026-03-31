4f28097b45119504b70963a79b92828576c49cfd
Dwinfly <dwinfly@gmail.com>
2026-03-31 22:50:09 +0300

    Add grid cursor indicator

--- Assets/Scripts/PlacementPreview.cs
+++ Assets/Scripts/PlacementPreview.cs
@@ -1,15 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlacementPreview : MonoBehaviour
 {
     [SerializeField] 
-    private GameObject mouseIndicator;
+    private GameObject mouseIndicator, cellIndicator;
     [SerializeField]
     private MouseRaycaster mouseRaycaster;
+    [SerializeField]
+    private Grid grid;
 
     private void Update()
     {
         Vector3 mousePosition = mouseRaycaster.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+
         mouseIndicator.transform.position = mousePosition;
-    } 
+        cellIndicator.transform.position = grid.GetCellCenterWorld(gridPosition);
+    }
 }
