533bf0e7ac99087a435f0d97a627d05a7ec241cd
Dwinfly <dwinfly@gmail.com>
2026-04-05 23:49:49 +0300

    Add function in PlaceableObject to check if object can place due to logic (and fix some bugs)

--- Assets/Scripts/Core/Common/PlaceableObject.cs
+++ Assets/Scripts/Core/Common/PlaceableObject.cs
@@ -4,6 +4,11 @@ public abstract class PlaceableObject : MonoBehaviour
 {
     public abstract Vector2Int Size { get; }
 
+    public virtual bool CanPlaceOn(CellType cellType)
+    {
+        return true;
+    }
+
     public virtual void OnPlaced(Vector3Int gridPosition)
     {
     }
